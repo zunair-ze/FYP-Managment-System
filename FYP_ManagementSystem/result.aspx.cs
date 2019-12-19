e633523e79e4134f1cb85d23dfa353b25ad28aac
Muhammad Zunair <57988413+ZunairZe@users.noreply.github.com>
2019-12-19 19:35:03 +0500

    Updation
    Updation

--- FYP_ManagementSystem/result.aspx.cs
+++ FYP_ManagementSystem/result.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,6 +6,9 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Data;
 using System.Data.SqlClient;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.html.simpleparser;
 
 namespace FYP_ManagementSystem
 {
@@ -35,6 +38,44 @@ namespace FYP_ManagementSystem
         protected void result_SelectedIndexChanged(object sender, EventArgs e)
         {
 
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            ///Response.Redirect("result.aspx");
+
+            PdfPTable pdftable = new PdfPTable(grid1.HeaderRow.Cells.Count);
+            foreach (TableCell headerCell in grid1.HeaderRow.Cells)
+            {
+                Font font = new Font();
+                font.Color = new BaseColor(grid1.HeaderStyle.ForeColor);
+                PdfPCell pdfCell = new PdfPCell(new Phrase(headerCell.Text, font));
+                pdftable.AddCell(pdfCell);
+            }
+            foreach (GridViewRow gridViewRow in grid1.Rows)
+            {
+                foreach (TableCell tableCell in gridViewRow.Cells)
+                {
+                    PdfPCell pdfCell = new PdfPCell(new Phrase(tableCell.Text));
+                    pdftable.AddCell(pdfCell);
+                }
+            }
+            Document pdfDocument = new Document(PageSize.A4, 10f, 10f, 10f, 10f);
+            PdfWriter.GetInstance(pdfDocument, Response.OutputStream);
+
+            pdfDocument.Open();
+            pdfDocument.Add(pdftable);
+            pdfDocument.Close();
+
+            Response.ContentType = "application/pdf";
+            Response.AppendHeader("content-disposition", "attachement;filename=Results.pdf");
+            Response.Write(pdfDocument);
+            Response.Flush();
+            Response.End();
+        }
+
+        protected void Button1_Click1(object sender, EventArgs e)
+        {
+            Response.Redirect("home.aspx");
         }
     }
 }
