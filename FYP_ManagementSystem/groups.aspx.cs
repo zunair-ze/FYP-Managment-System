5241999f6f33031cfbe1517d53053007b1be3fbd
Muhammad Zunair <57988413+ZunairZe@users.noreply.github.com>
2019-12-19 19:32:16 +0500

    Updation
    Updation

--- FYP_ManagementSystem/groups.aspx.cs
+++ FYP_ManagementSystem/groups.aspx.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -19,38 +19,60 @@ namespace FYP_ManagementSystem
                 conn.Close();
             }
             conn.Open();
-            SqlCommand cmd = conn.CreateCommand();
-            cmd.CommandText = "select * from members where type='student'";
-            name1.DataSource = cmd.ExecuteReader();
-            name1.DataBind();
-            conn.Close();
-            conn.Open();
 
-            cmd.CommandText = "select * from members where type='student'";
-            name2.DataSource = cmd.ExecuteReader();
-            name2.DataBind();
-            conn.Close();
-            conn.Open();
-
-            cmd.CommandText = "select * from members where type='advisor'";
-            advisor.DataSource = cmd.ExecuteReader();
-            advisor.DataBind();
-            conn.Close();
-            conn.Open();
-
-            cmd.CommandText = "select * from projects";
-            project.DataSource = cmd.ExecuteReader();
-            project.DataBind();
-            conn.Close();
-            conn.Open();
+            if (!IsPostBack)
+            {
+                showName();
+                showAdvisor();
+                showProject();
+            }
         }
 
         protected void add_Click(object sender, EventArgs e)
         {
             SqlCommand cmd = conn.CreateCommand();
-            cmd.CommandText = "insert into groups values('" + name1.SelectedValue + ',' + name2.SelectedValue + "','" + project.SelectedValue + "','" + advisor.SelectedValue + "')";
+            cmd.CommandText = "insert into groups values('" + ddlCountry.SelectedValue + ',' + name2.SelectedValue + "','" + project.SelectedValue + "','" + advisor.SelectedValue + "')";
             cmd.ExecuteNonQuery();
-            name1.SelectedValue = "";
+        }
+        protected void showName()
+        {
+            SqlCommand cmd = new SqlCommand("select * from members where type='student'", conn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            name2.DataSource = ddlCountry.DataSource = ds;
+            name2.DataTextField = ddlCountry.DataTextField = "name";
+            ddlCountry.DataBind();
+            name2.DataBind();
+            ddlCountry.Items.Insert(0, new ListItem("Select first Student", "0"));
+            name2.Items.Insert(0, new ListItem("Select second Student", "0"));
+        }
+        protected void showAdvisor()
+        {
+            SqlCommand cmd = new SqlCommand("select * from members where type='advisor'", conn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            advisor.DataSource = ds;
+            advisor.DataTextField = "name";
+            advisor.DataBind();
+            advisor.Items.Insert(0, new ListItem("Select Advisor", "0"));
+        }
+        protected void showProject()
+        {
+            SqlCommand cmd = new SqlCommand("select * from projects", conn);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            project.DataSource = ds;
+            project.DataTextField = "name";
+            project.DataBind();
+            project.Items.Insert(0, new ListItem("Select Project", "0"));
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("home.aspx");
         }
     }
 }
